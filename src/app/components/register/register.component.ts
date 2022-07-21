34acbaa10536eb688ec17531df2321bbf9a5c126
Marwa <koroko123430@gmail.com>
2022-07-21 23:22:12 +0300

    register + add cart to user interface

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+
+  name!: string;
+  email!: string;
+  password!: string;
+  err = false;
+  errMessage = "";
+  constructor(private _AuthService: AuthService
+    ,private _router: Router) { }
 
   ngOnInit(): void {
   }
 
+  submitted() {
+    if (!this.name || !this.email || !this.password)
+     return this.showErrorMessage("All Field are required")
+
+      this._AuthService.register(
+        { 'name': this.name, 'email': this.email, 'password': this.password })
+        .subscribe(res => { this._AuthService.saveUserData(res.user,res.token)
+          this._router.navigate(['./main'])},
+        err => this.showErrorMessage(err.error.message)
+        )
+
+  }
+
+  showErrorMessage(message:string){
+    this.err = true;
+    this.errMessage =message;
+  }
 }
--- src/app/interfaces/user.ts
+++ src/app/interfaces/user.ts
@@ -3,4 +3,5 @@ export interface IUser {
   email: string,
   name: string,
   token: string,
+  cart:[]
 }
