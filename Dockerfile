abfc4afbc6187097124343997a8dee38b65043ea
Marwa <koroko123430@gmail.com>
2022-08-03 21:48:40 +0300

    last commit

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM nginx:1.17.1-alpine
+# Hi
 COPY nginx.conf /etc/nginx/nginx.conf
 COPY /dist/ang-eshop /usr/share/nginx/html
--- src/app/common/app.util.ts
+++ src/app/common/app.util.ts
@@ -1,3 +1,3 @@
 export const AUTH_TOKEN = 'authToken';
 export const USER_INFO = 'mean_user';
-export const API_LINK = "https://node-e-shop-api.herokuapp.com/"
+export const API_LINK = "http://3.84.86.161/"
